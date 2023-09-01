8636e49f065d5d043c0bb937e893b9f4965bc4a6
Dmitry45667 <141731279+Dmitry45667@users.noreply.github.com>
2023-09-01 18:02:35 +0300

    Add files via upload

---
+++ HomeworkNaydenow/src/test/java/JdbcTest/FoodDB.java
@@ -0,0 +1,72 @@
+package JdbcTest;
+
+import DBconfig.ConnectionPool;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class FoodDB {
+
+    // Метод для добавления товара в таблицу FOOD
+    public void insertFood(String foodName, String foodType, int foodExotic) {
+        try (Connection connection = ConnectionPool.getDataSource().getConnection();
+             PreparedStatement ps = connection.prepareStatement(
+                     "INSERT INTO FOOD (FOOD_NAME, FOOD_TYPE, FOOD_EXOTIC) VALUES (?, ?, ?)")) {
+
+            ps.setString(1, foodName);
+            ps.setString(2, foodType);
+            ps.setInt(3, foodExotic);
+            ps.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        }
+    }
+
+    // Метод для просмотра содержимого таблицы FOOD
+    public void displayFoodTable() {
+        try (Connection connection = ConnectionPool.getDataSource().getConnection();
+             PreparedStatement ps = connection.prepareStatement("SELECT * FROM FOOD");
+             ResultSet rs = ps.executeQuery()) {
+
+            while (rs.next()) {
+                int foodId = rs.getInt("FOOD_ID");
+                String foodName = rs.getString("FOOD_NAME");
+                String foodType = rs.getString("FOOD_TYPE");
+                int foodExotic = rs.getInt("FOOD_EXOTIC");
+                System.out.println(" ID: " + foodId +
+                        " NAME: " + foodName +
+                        " TYPE: " + foodType +
+                        " isEXOTIC: " + foodExotic);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Метод для удаления товара из таблицы FOOD
+    public void deleteGoodById(int id) {
+
+        String deleteGoodRequest = "DELETE FROM FOOD WHERE FOOD_ID = ?";
+
+        try (Connection connection = ConnectionPool.getDataSource().getConnection();
+             PreparedStatement ps = connection.prepareStatement(deleteGoodRequest)) {
+
+            ps.setInt(1, id);
+
+            int rowsAffected = ps.executeUpdate();
+
+            if (rowsAffected > 0) {
+                System.out.println("Позиция успешно удалена из базы данных.");
+            } else {
+                System.out.println("Не удалось удалить позицию из базы данных. Возможно, товар с указанным ID не существует.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
